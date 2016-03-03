344472594385008c54c622b2c515148dc08e6bae
averywallis <averybwallis@gmail.com>
2016-03-03 14:45:06 -0500

    Updated from Brython Server: 3/3/2016 2:45:06 PM

--- cryptography.py
+++ cryptography.py
@@ -29,6 +29,7 @@ if ip=='e':
         thing.append(associations.find(key[y]))
     b=l-k
     thing.append(associations.find(key[-b]))
+    print(let)
     print(thing)
 elif ip=='d':
         print(let)

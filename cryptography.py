bc9d5ff21f688820910128e5752163f4cabd04f3
averywallis <averybwallis@gmail.com>
2016-03-03 14:20:39 -0500

    Updated from Brython Server: 3/3/2016 2:20:39 PM

--- cryptography.py
+++ cryptography.py
@@ -16,15 +16,16 @@ ip=input('Enter e to encrypt, d to decrypt, or q to quit: ')
 if not ip=='q' or ip=='e' or ip=='d':
     print("Did not understand command, try again.")
 ip=input('Enter e to encrypt, d to decrypt, or q to quit: ')
+
 if ip=='e':
     message=input("Message: ")
     key=str(input("Key: "))
     l=len(message)
     for x in range (0,l):
         let.append(associations.find(message[x]))
+        
 elif ip=='d':
         print(let)
 elif ip=='q':
-    print(let)
-elif not ip=='q' or ip=='e' or ip=='d':
-    print("Did not understand command, try again.")
+    break
+

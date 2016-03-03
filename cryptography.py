ca104ff4577ec3ff743f908674a415fb9b8f5ad8
averywallis <averybwallis@gmail.com>
2016-03-03 14:09:58 -0500

    Updated from Brython Server: 3/3/2016 2:09:58 PM

--- cryptography.py
+++ cryptography.py
@@ -12,17 +12,14 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 let=[]
 ip=input('Enter e to encrypt, d to decrypt, or q to quit: ')
-
-if ip=='e':
-    message=input("Message: ")
-    key=str(input("Key: "))
-    l=len(message)
-    for x in range (0,l):
-        let.append(associations.find(message[x]))
-elif ip=='d':
-    print(let)
-elif ip=='q':
-    print(let)
-
-
-print(let)
+for y in range(0,100):
+    if ip=='e':
+        message=input("Message: ")
+        key=str(input("Key: "))
+        l=len(message)
+        for x in range (0,l):
+            let.append(associations.find(message[x]))
+    elif ip=='d':
+        print(let)
+    elif ip=='q':
+        print(let)
